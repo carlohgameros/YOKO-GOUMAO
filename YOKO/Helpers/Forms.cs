570b3012529c7d5d317f2964053580b8b5878ed7
Carlo Gameros <carlo.hernandez16@tectijuana.edu.mx>
2019-07-24 18:06:59 -0700

    Agregando el facilitador para las formas

---
+++ YOKO/Helpers/Forms.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YOKO.Helpers
+{
+    public class Forms
+    {
+        public static Form form = new Form();
+
+        public static void GoToNextForm(Form actualForm, Form nextForm)
+        {
+            form = nextForm;
+            form.Show();
+            actualForm.Close();
+        }
+
+       public static void GoToPreciousForm(Form actualForm)
+        {
+            form.Show();
+            var temporaryForm = actualForm;
+            actualForm.Close();
+            form = temporaryForm;
+        }
+    }
+}
